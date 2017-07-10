4d3f5f2bd856658aa59c0f95273da5e5d89e2425
wetalk <tradp21@gmail.com>
2017-07-11 00:42:01 +0800

    Create ko-KR.js
    韩文翻译。

---
+++ src/locale/lang/ko-KR.js
@@ -0,0 +1,96 @@
+export default {
+    i: {
+        select: {
+            placeholder: '선택하세요',
+            noMatch: '해당 데이터가 없습니다',
+            loading: '로딩중'
+        },
+        table: {
+            noDataText: '데이터가 없습니다',
+            noFilteredDataText: '일치하는 데이터가 없습니다',
+            confirmFilter: '필터',
+            resetFilter: '초기화',
+            clearFilter: '전체'
+        },
+        datepicker: {
+            selectDate: '날짜선택',
+            selectTime: '시간선택',
+            startTime: '시작시간',
+            endTime: '종료시간',
+            clear: '비우기',
+            ok: '확인',
+            month: '월',
+            month1: '1 월',
+            month2: '2 월',
+            month3: '3 월',
+            month4: '4 월',
+            month5: '5 월',
+            month6: '6 월',
+            month7: '7 월',
+            month8: '8 월',
+            month9: '9 월',
+            month10: '10 월',
+            month11: '11 월',
+            month12: '12 월',
+            year: '년',
+            weeks: {
+                sun: '일',
+                mon: '월',
+                tue: '화',
+                wed: '수',
+                thu: '목',
+                fri: '금',
+                sat: '토'
+            },
+            months: {
+                m1: '1월',
+                m2: '2월',
+                m3: '3월',
+                m4: '4월',
+                m5: '5월',
+                m6: '6월',
+                m7: '7월',
+                m8: '8월',
+                m9: '9월',
+                m10: '10월',
+                m11: '11월',
+                m12: '12월'
+            }
+        },
+        transfer: {
+            titles: {
+                source: '원본목록',
+                target: '타겟목록'
+            },
+            filterPlaceholder: '검색 내용을 입력하세요',
+            notFoundText: '일치하는 데이터가 없습니다'
+        },
+        modal: {
+            okText: '확인',
+            cancelText: '취소'
+        },
+        poptip: {
+            okText: '확인',
+            cancelText: '취소'
+        },
+        page: {
+            prev: '이전',
+            next: '다음',
+            total: '총',
+            item: '건',
+            items: '건',
+            prev5: '이전 5 페이지',
+            next5: '다음 5 페이지',
+            page: '건/페이지',
+            goto: '',
+            p: '페이지로'
+        },
+        rate: {
+            star: '포인트',
+            stars: '포인트'
+        },
+        tree: {
+            emptyText: '데이터가 없습니다'
+        }
+    }
+};
